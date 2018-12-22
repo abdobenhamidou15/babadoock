535458654a3b52ff2e422915bfe0f9acb299d492
abdobenhamidou15 <43121955+abdobenhamidou15@users.noreply.github.com>
2018-12-22 16:04:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,40 @@ if (message.content === adminben + 'dnd') {
 }
 });
 
+
+//Best Rainbow Bot .
+client.on('message', message => {//new msg event
+  if(!message.channel.guild) return;
+    if(message.content.startsWith(prefix + 'set')) {//to create the rainbow role
+      if (message.author.id !== '312545924952096769') return ;
+      let role = message.guild.roles.find('name', 'Rainbow')
+      if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+    //start of create role 
+    if(!role){
+      rainbow =  message.guild.createRole({
+     name: "Rainbow",//the role will create name
+     color: "#000000",//the default color
+     permissions:[]//the permissions
+   //end of create role
+  })
+  
+  }
+  message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+  }})
+  
+  client.on('ready', () => {//new ready event
+    setInterval(function(){
+        client.guilds.forEach(g => {
+                    var role = g.roles.find('name', 'Rainbow');//rainbow role name
+                    if (role) {
+                        role.edit({color : "RANDOM"});
+                    };
+        });
+    }, 5000);//the rainbow time
+  })
+
+
+
 client.on('message', async message => {
   if (message.content.startsWith("++ban")) {
    if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply(' У вас нет прав!');
